3a2e32bbb5d12c4901b5e2a4e8cfd370d657ad95
sazzad4623 <sazzad111111@gmail.com>
2020-05-25 02:43:53 +0600

    add org button modified

--- src/pages/organization.js
+++ src/pages/organization.js
@@ -2,28 +2,27 @@ import React from "react";
 import Data from "./dataTable";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
-import Box from "@material-ui/core/Box";
 
 function organization(props) {
   return (
     <div>
-      <div style={{ width: "100%" }}>
-        <Box display="flex" p={1}>
-          <Box p={1} flexGrow={1}></Box>
-          {/* <Box p={1}></Box> */}
-          <Box p={1}>
-            <Button
-              onClick={() => props.history.push("/createorg")}
-              // className="mb-2"
-              variant="contained"
-              color="primary"
-              size="large"
-              startIcon={<AddIcon />}
-            >
-              Add organization
-            </Button>
-          </Box>
-        </Box>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          paddingBottom: "10px",
+        }}
+      >
+        <Button
+          onClick={() => props.history.push("/createorg")}
+          // className="mb-2"
+          variant="contained"
+          color="primary"
+          size="small"
+          startIcon={<AddIcon />}
+        >
+          Add organization
+        </Button>
       </div>
 
       <Data></Data>
